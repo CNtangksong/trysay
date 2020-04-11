6eace1229fb9d1c0e6b1e7fab61eac376dc8c1b2
CNtangksong <1037332097@qq.com>
2020-04-11 23:35:36 +0800

    use mybatis generator to rebuilt mapper and mode

--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -5,7 +5,9 @@ import com.example.demo.dto.QuestionDTO;
 import com.example.demo.mapper.QuestionMapper;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.model.Question;
+import com.example.demo.model.QuestionExample;
 import com.example.demo.model.User;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +27,7 @@ public class QuestionService {
     public PaginationDTO list(Integer page, Integer size) {
 
         PaginationDTO paginationDTO = new PaginationDTO();
-        Integer totalCount = questionMapper.count();
+        Integer totalCount = (int)questionMapper.countByExample(new QuestionExample());
 
         Integer totalPage;
 
@@ -43,9 +45,8 @@ public class QuestionService {
         }
 
         paginationDTO.setPagination(totalPage,page);
-
         Integer offset = size *(page-1);
-        List<Question> questions=questionMapper.list(offset,size);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, size));//利用mybatis的一个插件实现分页
         List<QuestionDTO> questionDTOList = new ArrayList<>();//新建dtoList
 
         for (Question question:questions) {
@@ -63,9 +64,13 @@ public class QuestionService {
 
     public PaginationDTO list(Integer userId, Integer page, Integer size) {
         PaginationDTO paginationDTO = new PaginationDTO();
-        Integer totalCount = questionMapper.countByUserId(userId);
 
         Integer totalPage;
+
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.createCriteria()
+                .andCreatorEqualTo(userId);//查userId
+        Integer totalCount = (int)questionMapper.countByExample(questionExample);
 
         if(totalCount % size==0){//算总页数
             totalPage=totalCount / size ;
@@ -83,7 +88,11 @@ public class QuestionService {
         paginationDTO.setPagination(totalPage,page);
 
         Integer offset = size *(page-1);
-        List<Question> questions=questionMapper.listByUserId(userId,offset,size);
+
+        QuestionExample example=new QuestionExample();
+        example.createCriteria()
+                .andCreatorEqualTo(userId);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(example, new RowBounds(offset, size));//利用mybatis的一个插件实现分页
         List<QuestionDTO> questionDTOList = new ArrayList<>();//新建dtoList
 
         for (Question question:questions) {
@@ -100,7 +109,7 @@ public class QuestionService {
 
 
     public QuestionDTO getById(Integer id) {
-        Question question = questionMapper.getById(id);
+        Question question = questionMapper.selectByPrimaryKey(id);
         QuestionDTO questionDTO = new QuestionDTO();
         BeanUtils.copyProperties(question,questionDTO);//question放到DTO
         User user =userMapper.selectByPrimaryKey(question.getCreator());
@@ -112,10 +121,17 @@ public class QuestionService {
         if(question.getId()==null){//创建
             question.setGmtCreate(System.currentTimeMillis());
             question.setGmtModified(question.getGmtCreate());
-            questionMapper.creat(question);
+            questionMapper.insert(question);
         }else{//更新
-            question.setGmtModified(System.currentTimeMillis());
-            questionMapper.update(question);
+            Question updateQuestion = new Question();
+            updateQuestion.setGmtModified(System.currentTimeMillis());
+            updateQuestion.setTitle(question.getTitle());
+            updateQuestion.setDescription(question.getDescription());
+            updateQuestion.setTag(question.getTag());
+            QuestionExample example = new QuestionExample();
+            example.createCriteria()
+                    .andIdEqualTo(question.getId());
+            questionMapper.updateByExampleSelective(updateQuestion, example);
 
         }
     }
