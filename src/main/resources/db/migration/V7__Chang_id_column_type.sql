e72cda6aa2242769aed070b67fd93330b9b046b7
CNtangksong <1037332097@qq.com>
2020-04-24 23:49:40 +0800

    change the  mysql

--- src/main/resources/db/migration/V7__Chang_id_column_type.sql
+++ src/main/resources/db/migration/V7__Chang_id_column_type.sql
@@ -1,2 +1,4 @@
-alter table QUESTION alter column id BIGINT auto_increment;
-alter table USER alter column id BIGINT auto_increment;
+alter table QUESTION
+    alter column id BIGINT auto_increment;
+alter table USER
+    alter column id BIGINT auto_increment;
--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,4 @@
-alter table QUESTION alter column creator BIGINT auto_increment;
-alter table COMMENT alter column commentator BIGINT auto_increment;
+alter table QUESTION
+    alter column creator BIGINT auto_increment;
+alter table COMMENT
+    alter column commentator BIGINT auto_increment;
